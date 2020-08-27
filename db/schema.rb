492d8fe838b55f445a7d49fed61b19b10a1ad23b
Александр Фалько <aleksandrfalko@MacBook-Pro-Aleksandr.local>
2020-08-27 12:39:44 +0300

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_17_082615) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "status"
