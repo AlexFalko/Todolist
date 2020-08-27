3e6fb5bab44ba7d0534f97cd3b2d18b833686a37
Александр Фалько <aleksandrfalko@MacBook-Pro-Aleksandr.local>
2020-08-27 17:03:38 +0300

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   
   root 'todolists#index'
-  # get 'persons/profile', as: 'user_root'
   devise_for :users
-  
-  
   resources :todolists do
      resources :tasks 
-    end
-  
-
+  end
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
