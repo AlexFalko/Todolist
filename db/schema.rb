37cb54bd1f2686cb3533f9957e60f1d262ba3abf
Alexander <sashafalko129@gmail.com>
2020-08-30 19:43:16 +0300

    Change migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_082615) do
+ActiveRecord::Schema.define(version: 2020_08_10_202625) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "status"
     t.datetime "date_end"
-    t.integer "todolist_id", null: false
+    t.bigint "todolist_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_082615) do
   create_table "todolists", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_todolists_on_user_id"
